e6dcc4a11a5050458d1d382afaf63ba7f419d60b
benjazzy <benjazzy64@gmail.com>
2020-10-24 03:39:08 -0400

    Implemented minimax search instead of averaging points.

--- src/lib.rs
+++ src/lib.rs
@@ -24,8 +24,8 @@ static OUTCOMES: Outcomes = Outcomes {
     draw: 500.0,
     my_checkmate: 1000.0,
     my_check: 100.0,
-    their_checkmate: -1000.0,
-    their_check: -100.0,
+    their_checkmate: 1000.0,
+    their_check: 100.0,
     queen: 100.0,
     knight: 50.0,
     bishop: 50.0,
@@ -34,7 +34,7 @@ static OUTCOMES: Outcomes = Outcomes {
 };
 
 const NUMTHREADS: usize = 8;            //NUMTHREADS is the number of threads to run the ai on.
-const MAXDEPTH: isize = 5;              //The max number of moves run.
+const MAXDEPTH: isize = 4;              //The max number of moves run.
 const OUTCOMESMULTIPLIER: f64 = 2.5;    //Number to multiply the outcomes by.
 // const NEXTMOVEMULTIPLIER: f64 = 2.0; //A higher number increases the points that
 const TAKEPIECEMULTIPLIER: f64 = 1.01;  //Applied when taking a piece not when losing a piece.
@@ -57,69 +57,44 @@ pub fn do_move(board: Box<Board>, color: Color, previous_boards: Arc<Vec<BitBoar
     let (tx, rx) = channel();
     //Loop through each possible move.
     for i in it {
-        //Clone variables needed in the thread.
         let tx = tx.clone();
         let board = board.clone();
         let previous_boards:Arc<Vec<BitBoard>> = Arc::clone(&previous_boards);
-        //Start the thread.
+
         pool.execute(move|| {
-            //get the target piece of the move.  If the move is not going take a piece the target will be None
-            let target = board.piece_on(i.get_dest());
-            //Make the move i and put the resulting board in result.
+            let target = board.piece_on(i.get_dest());  //Gets the target piece of the move.  None if there is no target piece.
+
+            //Make a the move and put the board in result.
             let mut result = Board::from(*board.clone());
             board.make_move(i, &mut result);
 
-            //Current score of the move.  The score of this move and the scores of consecutive moves get added to local_score.
-            let mut local_score: f64 = 0.0;
+            let mut score = 0.0;
 
-            //Check if target is something and if the move involes the ai's color taking a piece then and the points to local_score.
-            //Otherwise if it is not the ai's move then subtract the points from local_score.
-            //If there is no piece being taken do nothing.
             match target {
                 Some(t) => {
-                    if color == board.side_to_move() {
-                        local_score += match_piece(t) * OUTCOMESMULTIPLIER * TAKEPIECEMULTIPLIER;
-                    } else {
-                        local_score -= match_piece(t) * OUTCOMESMULTIPLIER;
-                    }
+                    score += match_piece(t);
                 },
-                None => {},
-            }
-            //If a move's outcome is checkmate add or subtract the points from local_score depending on who is making the move.
-            //If there is not a checkmate do noting.
+                None => {}
+            };
             match board.status() {
                 BoardStatus::Checkmate => {
-                    if board.side_to_move() == color {
-                        local_score += OUTCOMES.my_checkmate * OUTCOMESMULTIPLIER;
-                    } else {
-                        local_score += OUTCOMES.their_checkmate * OUTCOMESMULTIPLIER;
-                    }
+                    score += OUTCOMES.their_checkmate;
                 },
                 _ => {}
             };
 
-            //Call check_moves with the result.  This will continue the chain of checking moves.
-            //If check_moves returns a score add it to local_score.
-            match check_moves(Box::new(result), color, 1, previous_boards.clone()) {
-                Some((_, s, _)) => {
-                    if color == board.side_to_move() {
-                        local_score += s;
+            if !is_threefold(*result.combined(), previous_boards.clone()) {
+                match search_min(0.0, 0.0, Box::new(result), 1, score, previous_boards.clone()) {
+                    Some(s) => {
+                        // println!("Score: {}", s);
+                        tx.send(Some((i, s + score, 0)));
                     }
-                    // moves.push((i, local_score, d));
-                },
-                None => {
-                    // moves.push((i, local_score, 0));
-                },
-            };
-
-            //If this move does not break the threefold rule send the move and its score through tx.
-            //Otherwise send nothing.
-            if !is_threefold(*result.color_combined(color), previous_boards.clone()) {
-                tx.send(Some((i, local_score, 0)));
-            } else {
-                tx.send(None);
+                    None => {
+                        tx.send(None);
+                    }
+                }
             }
-        });
+        })
     }
 
     //Match the values sent by the txs into moves if the value sent by tx is not None.
@@ -142,29 +117,20 @@ pub fn do_move(board: Box<Board>, color: Color, previous_boards: Arc<Vec<BitBoar
                 max = i.1;
             }
         }
+        println!("Max Score: {}", max);
         Some(selected_move)
     } else { None }
 
     // return Some(ChessMove::new(Square::A1, Square::A2, None))
 }
 
-//Recursively check all moves possible on board and return the best move and the sum of all the scores.
-//Board is the current board to check, color is the color of the side the ai is on, depth is how far into the moves the ai is on to make sure the recursion dosn't go
-// on forever, boards is the list of previous board positions to enforce the threefold rule in the ai.
-fn check_moves(board: Box<Board>, color: Color, mut depth: isize,  boards: Arc<Vec<BitBoard>>) -> Option<(ChessMove, f64, isize)> {
-    //Add one the the depth and make sure we aren't to deep.  If depth is over the MAXDEPTH return None.
+fn search_max(mut alpha: f64, beta: f64, board: Box<Board>, mut depth: isize, total_score: f64, previous_boards: Arc<Vec<BitBoard>>) -> Option<f64> {
+    //Add one the the depth.
     depth += 1;
-    if depth > MAXDEPTH {
-        return None;
-    }
 
+    let it = MoveGen::new_legal(&board);
+    let mut scores = vec![];
 
-    let it = MoveGen::new_legal(&board);               //Get a iterator of next legal moves.
-    let mut moves: Vec<(ChessMove, f64, isize)> = vec![];       //Initialize a vector of empty moves and points that the move could lead to.
-    let max_depth = depth;                                //Currently not used.  TODO remember why I put this in here.
-    let mut local_score: f64 = 0.0;                             //Sum of this boards move scores and consecutive move scores.
-
-    //Iterate over all possible moves.
     for i in it {
         let target = board.piece_on(i.get_dest());  //Gets the target piece of the move.  None if there is no target piece.
 
@@ -172,62 +138,117 @@ fn check_moves(board: Box<Board>, color: Color, mut depth: isize,  boards: Arc<V
         let mut result = Board::from(*board.clone());
         board.make_move(i, &mut result);
 
-        //If there is a target piece then add or subtract the points form local_score depending on which color is making the move.
+        let mut score = 0.0;
+
         match target {
             Some(t) => {
-                if color == board.side_to_move() {
-                    local_score += match_piece(t) / depth as f64 * OUTCOMESMULTIPLIER;
-                } else {
-                    local_score -= match_piece(t) / depth as f64 * OUTCOMESMULTIPLIER;
-                }
+                score += match_piece(t);
             },
-            None => {},
-        }
+            None => {}
+        };
 
-        //If a move's outcome is checkmate add or subtract the points from local_score depending on who is making the move.
-        //If there is not a checkmate do noting.
-        match board.status() {
+        match result.status() {
             BoardStatus::Checkmate => {
-                if board.side_to_move() == color {
-                    local_score += OUTCOMES.my_checkmate / depth as f64 * OUTCOMESMULTIPLIER;
-                } else {
-                    local_score += OUTCOMES.their_checkmate / depth as f64 * OUTCOMESMULTIPLIER;
-                }
+                score += OUTCOMES.their_checkmate;
             },
             _ => {}
         };
 
-        //Recursivly call check_moves and if it returns something add the score to local_score.
-        match check_moves(Box::new(result), color, depth, boards.clone()) {
-            Some((_, s, _)) => {
-                if color == board.side_to_move() {
-                    local_score += s;
-                }
-            },
-            None => {
-            },
-        };
+        score += total_score;
 
-        //If the threefold criteria are not met then push the current move and score onto moves.
-        if !is_threefold(*result.color_combined(color), boards.clone()) {
-            moves.push((i, local_score, max_depth));
+        if !is_threefold(*result.combined(), previous_boards.clone()) {
+            if depth < MAXDEPTH {
+                match search_min(alpha, beta, Box::new(result), depth, score, previous_boards.clone()) {
+                    Some(s) => {
+                        score += s;
+                        scores.push(score);
+                        // if score >= beta {
+                        //     return Some(beta)
+                        // } else if score > alpha {
+                        //     alpha = score;
+                        // }
+                    },
+                    None => {}
+                };
+            } else {
+                scores.push(score);
+            }
         }
     }
 
-    //Check if moves is empty.  If so return None.
-    //Otherwise find the move with the highest score and return it and local_score.
-    if moves.len() > 0 {
-        let mut max: f64 = moves.get(0).unwrap().1;
-        let mut selected_move = moves.get(0).unwrap().0;
-        for i in moves {
-            if i.1 > max {
-                selected_move = i.0;
-                max = i.1;
+    if scores.len() > 0 {
+        let mut max = *scores.get(0).expect("Error unwrapping score.");
+        for score in scores {
+            if score > max {
+                max = score;
             }
         }
+        return Some(max);
+    }
+    return None;
+}
 
-        Some((selected_move, local_score, max_depth))
-    } else { None }
+fn search_min(alpha: f64, mut beta: f64, board: Box<Board>, mut depth: isize, total_score: f64, previous_boards: Arc<Vec<BitBoard>>) -> Option<f64> {
+    //Add one the the depth.
+    depth += 1;
+
+    let it = MoveGen::new_legal(&board);
+    let mut scores = vec![];
+
+    for i in it {
+        let target = board.piece_on(i.get_dest());  //Gets the target piece of the move.  None if there is no target piece.
+
+        //Make a the move and put the board in result.
+        let mut result = Board::from(*board.clone());
+        board.make_move(i, &mut result);
+
+        let mut score = 0.0;
+
+        match target {
+            Some(t) => {
+                score -= match_piece(t);
+            },
+            None => {}
+        };
+        match result.status() {
+            BoardStatus::Checkmate => {
+                score -= OUTCOMES.my_checkmate;
+            },
+            _ => {}
+        };
+
+        score += total_score;
+
+        if !is_threefold(*result.combined(), previous_boards.clone()) {
+            if depth < MAXDEPTH {
+                match search_max(alpha, beta, Box::new(result), depth, score, previous_boards.clone()) {
+                    Some(s) => {
+                        score += s;
+                        scores.push(score);
+                        // if score <= alpha {
+                        //     return Some(alpha)
+                        // } else if score < beta {
+                        //     beta = score;
+                        // }
+                    },
+                    None => {}
+                };
+            } else {
+                scores.push(score);
+            }
+        }
+    }
+
+    if scores.len() > 0 {
+        let mut min = *scores.get(0).expect("Error unwrapping score.");
+        for score in scores {
+            if score < min {
+                min = score;
+            }
+        }
+        return Some(min);
+    }
+    return None;
 }
 
 //Check to make see if the BitBoard of board is found in boards more than once.
